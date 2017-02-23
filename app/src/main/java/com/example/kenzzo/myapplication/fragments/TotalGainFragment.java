56475828998f05f841eed57a93c3a42fc88467b7
Benoit.PUECH <benoitpuech@macbook-pro-de-benoitpuech.home>
2017-02-23 02:43:22 +0100

    fragment main

---
+++ app/src/main/java/com/example/kenzzo/myapplication/fragments/TotalGainFragment.java
@@ -0,0 +1,22 @@
+package com.example.kenzzo.myapplication.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.kenzzo.myapplication.R;
+
+public class TotalGainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_total_gain_fragment, container, false);
+
+        return view;
+
+    }
+}
